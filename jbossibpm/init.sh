70e06025fbb6ed23decd61d929e4f56a1c94f9be
gkranenborg <gerben.kranenborg@us.fujitsu.com>
2018-05-15 10:05:08 -0400

    Daily update

--- jbossibpm/init.sh
+++ jbossibpm/init.sh
@@ -2,17 +2,24 @@
 
 su enterprisedb -c '/opt/edb/9.5AS/bin/pg_ctl start -D /opt/edb/9.5AS/data'
 
-# HOST_ADDR=$(hostname -i)
-HOST_ADDR="127.0.0.1"
-# HOST_NAME=$(hostname -s)
-HOST_NAME="localhost"
+sleep 10
+
+if [ -f /opt/engine/server/instance/default/console/conf/console.conf ]
+then
+	HOST_ADDR=$(hostname -i)
+	/hostname.sh
+else
+	HOST_ADDR="127.0.0.1"
+fi
+
+HOST_NAME=$(hostname -s)
 
 # some properties to avoid JGroups issues when using cluster/ha profiles
 JVM_PROPS="-Djboss.node.name=$HOST_NAME -Djgroups.bind_addr=$HOST_ADDR"
 # when using mod_cluster, change the default node name
 JVM_PROPS="$JVM_PROPS -Djboss.mod_cluster.jvmRoute=$HOST_NAME -DjvmRoute=$HOST_NAME"
 
-# bind the public interface to 0.0.0.0 due a issue with mod_cluster.
-$JBOSS_HOME/bin/domain.sh -b 0.0.0.0 -bunsecure=$HOST_ADDR -bmanagement=$HOST_ADDR $JVM_PROPS &
+# Starting JBoss.
+$JBOSS_HOME/bin/domain.sh -b $HOST_ADDR -bunsecure=$HOST_ADDR -bmanagement=$HOST_ADDR $JVM_PROPS &
 
 exec "$@"
